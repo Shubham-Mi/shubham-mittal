8637b53211b93b2b11a3f7d8878f69f42e92fb45
Shubham Mittal <shubham.mitt112@gmail.com>
2024-10-02 18:15:19 +0530

    added base CSS

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+/**
+ * @copyright 2024 Shubham-Mi
+ * @license Apache-2.0
+ */
+
+const App = () => {
+  return (
+    <div>
+      <h1>Shubham Mittal</h1>
+    </div>
+  );
+};
+
+export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,15 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+/**
+ * @copyright 2024 Shubham-Mi
+ * @license Apache-2.0
+ */
 
-createRoot(document.getElementById('root')).render(
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
     <App />
-  </StrictMode>,
-)
+  </StrictMode>
+);
