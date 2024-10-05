cd3e63844fd2eaf36782fec759deefd9371a3e19
Shubham Mittal <shubham.mitt112@gmail.com>
2024-10-05 14:23:42 +0530

    added contact me section

--- src/App.jsx
+++ src/App.jsx
@@ -11,6 +11,7 @@ import Hero from "./components/Hero";
 import About from "./components/About";
 import Skills from "./components/Skills";
 import Projects from "./components/Projects";
+import Contact from "./components/Contact";
 
 const App = () => {
   return (
@@ -21,6 +22,7 @@ const App = () => {
         <About />
         <Skills />
         <Projects />
+        <Contact />
       </main>
     </>
   );
